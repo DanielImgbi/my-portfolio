bf2cede94b79c43786f13da954f88f008b4e95a4
DanielImgbi <imgbidanielgodspower@gmail.com>
2023-08-29 17:00:07 +0500

    adding nav

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <div className="">
       <NavBar />
       <h1 className="h1">Hello World</h1>
     </div>
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import { FaHamburger, FaHome, FaBrain, FaFile, FaPhone } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className=" px-5 py-4 h-20 flex justify-between items-center">
-      <div className=" p-4">
-        <span className="text-3xl text-blue-600 font-bold mx-2 border border-white px-4">
+    <div className=" px-5 py-4 h-20 flex justify-between items-center ">
+      <div className=" shadow-md">
+        <span className="text-3xl bg-blue-600 text-white rounded-tr-full font-bold border border-white px-4 py-2 shadow-md">
           D
         </span>
-        <span className="text-3xl font-semibold text-ellipsis "> aniel</span>
+        <span className="text-4xl font-semibold text-ellipsis pr-1 ">
+          aniel
+        </span>
       </div>
 
-      <nav>
-        <ol className="lg:flex justify-between items-center bg-green-900 hidden">
-          <li>home</li>
-          <li>My Experience</li>
-          <li>My Portfolio</li>
-          <li>Contacts</li>
-        </ol>
-      </nav>
+      <div className="flex">
+        <FaHamburger
+          className={`text-3xl font-semibold text-ellipsis lg:hidden `}
+        />
+
+        <nav
+          className={`absolute top-20 w-full lg:w-20 lg:rounded-md lg:py-2 lg:fixed lg:top-80 lg:right-0 lg:shadow-lg `}
+        >
+          <ol className="flex flex-col justify-around text-3xl font-semibold px-2  bg-white  h-[250px] lg:justify-between lg:items-center ">
+            <li>
+              <a href="#" className="flex items-center">
+                <FaHome className="text-blue-600 mr-2 " />{" "}
+                <span className="lg:hidden">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center">
+                <FaBrain className="text-blue-600 mr-2" />{" "}
+                <span className="lg:hidden">My Experience</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center">
+                <FaFile className="text-blue-600 mr-2" />{" "}
+                <span className="lg:hidden">My Portfolio</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center">
+                <FaPhone className="text-blue-600 mr-2" />{" "}
+                <span className="lg:hidden">Contacts</span>
+              </a>
+            </li>
+          </ol>
+        </nav>
+      </div>
     </div>
   );
 };
