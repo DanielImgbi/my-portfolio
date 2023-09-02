1e42e5f00912ba1fbfd4d68bff171dbd3a9c06e2
DanielImgbi <102862847+DanielImgbi@users.noreply.github.com>
2023-09-02 03:05:49 +0100

    updating app

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,14 @@
 import { Suspense, lazy } from "react";
 import Myloader from "./components/Myloader";
 
-// const Pages = lazy(() => import("./components/Pages"));
+const Pages = lazy(() => import("./components/Pages"));
 
 function App() {
-  return <Suspense fallback={<Myloader />}>{/* <Pages /> */}</Suspense>;
+  return (
+    <Suspense fallback={<Myloader />}>
+      <Pages />
+    </Suspense>
+  );
 }
 
 export default App;
